9eada97a6da992aeb201d2e517bdd9b5e6139ff1
benbarnett02 <46703294+benbarnett02@users.noreply.github.com>
2023-01-26 14:55:32 +1100

    dev

--- FlashCards/FlashCards/Database.cs
+++ FlashCards/FlashCards/Database.cs
@@ -14,19 +14,29 @@ namespace FlashCards
             public string Id { get; set; }
             public string Challenge { get; set; }
             public string Answer { get; set; }
-            public string Blah { get; set; }
         }
 
+        public class FlashCardDisplayFormat
+        {
+            public string Challenge { get; set; }
+            public string Answer { get; set; }
+        }
+
+        public class FlashCardStack
+        {
+            public string Id { get; set; }
+            public string StackName { get; set; }
+            public List<FlashCard> Cards { get; set; }
+           
+        }
         public static FlashCard GetFlashCard()
         {
             // The method to set the DTO?
             FlashCard thisFlashCard = new()
             {
-                Id = "1",
-                Challenge = "The question",
-                Answer = "The Answer",
-                Blah = "Another thing"
+               
             };
+
 
             return thisFlashCard;
         }
